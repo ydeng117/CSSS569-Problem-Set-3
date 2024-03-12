e9e3c6aa3ac0917e38f44bc858cf7d46e213f01c
Ydeng <ydeng117@uw.edu>
2024-03-11 21:14:55 -0700

    ver1.0.1

--- app.R
+++ app.R
@@ -6,6 +6,7 @@ library(ggpubr)
 library(RColorBrewer)
 library(firatheme)
 library(shiny)
+
 source("data_cleaning.R")
 
 
@@ -13,32 +14,36 @@ source("data_cleaning.R")
 ui <- fluidPage(
     # Application theme settings
     shinythemes::themeSelector(),
-
+    
     # Application title
-    titlePanel(""),
-
-    # Sidebar with a slider and select input for number of bins 
+    titlePanel("Relation between Total Fertility Rate and Women's Mean Education in Productive Age from 1970 to 2009"),
+    
+    # Sidebar with a slider and select input for number of bins
     sidebarLayout(
         sidebarPanel(
-          # choosing the years for the analysis
-            sliderInput("year",
-                        label = h3("Choose a year:"),
-                        min = 1970,
-                        max = 2021,
-                        value = 1996),
-          # Select a group of region for display
-          checkboxGroupInput("region_group",
-                             label = h3("Filter by region group:"),
-                             choices = unique(tfr_women_edu_df$four_regions),
-                             selected = "asia"),
-          hr(), # Add a horizontal rule
-          checkboxInput("smooth", "Add smoother")
+            # choosing the years for the analysis
+            sliderInput(
+                "year",
+                label = h3("Choose a year:"),
+                min = 1970,
+                max = 2009,
+                value = 1996
+            ),
+            # Select a group of region for display
+            checkboxGroupInput(
+                "region_group",
+                label = h3("Filter by region group:"),
+                choices = unique(tfr_women_edu_df$four_regions),
+                selected = "asia"
+            ),
+            hr(),
+            # Add a horizontal rule
+            checkboxInput("smooth", "Add smoother"),
+            checkboxInput("facet", "Small multiples")
         ),
-
+        
         # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+        mainPanel(plotOutput("distPlot"))
     )
 )
 
@@ -90,18 +95,27 @@ server <- function(input, output) {
                     segment.size = 0.2
                 ),
                 scale_color_brewer(palette = "Set1"),
-                labs(title = "Relation between Total Fertility Rate and Women's Mean Education in Productive Age in 2002",
-                     x = "Women's Mean Education in Productive Age (15 to 44)",
-                     y = "Total Fertility Rate (babies per women)",
-                     color = "Region"),
-                scale_x_continuous(breaks = seq(0,16, by = 2)),
-                scale_y_continuous(breaks = seq(0,10, by = 2),limits = c(-.1,9))
+                labs(
+                    title = "Relation between Total Fertility Rate and Women's Mean Education in Productive Age in 2002",
+                    x = "Women's Mean Education in Productive Age (15 to 44)",
+                    y = "Total Fertility Rate (babies per women)",
+                    color = "Region"
+                ),
+                scale_x_continuous(breaks = seq(0, 16, by = 2)),
+                scale_y_continuous(
+                    breaks = seq(0, 10, by = 2),
+                    limits = c(-.1, 9)
+                ),
+                
+                if (input$facet)
+                    facet_wrap( ~ four_regions)
                 
             )
+        
         p
         
     }, res = 100)
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
